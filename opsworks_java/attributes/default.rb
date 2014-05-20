fe3d7fdacc4ae0e47df49d3610bb9e433e3a6092
jonasschultzmblox <jonasschultzmblox@users.noreply.github.com>
2014-05-20 14:39:27 +0200

    Update default.rb

--- opsworks_java/attributes/default.rb
+++ opsworks_java/attributes/default.rb
@@ -16,19 +16,24 @@
 
 default['opsworks_java'] = {}
 
-default['opsworks_java']['jvm'] = 'openjdk'
-default['opsworks_java']['jvm_version'] = '7'
+
+#default['opsworks_java']['jvm'] = 'openjdk'
+#default['opsworks_java']['jvm_version'] = '7'
+#default['opsworks_java']['jvm_options'] = ''
+default['opsworks_java']['jvm'] = 'jdk'
+default['opsworks_java']['jvm_version'] = '7u55'
 default['opsworks_java']['jvm_options'] = ''
 
+
 default['opsworks_java']['jvm_pkg'] = {}
-default['opsworks_java']['jvm_pkg']['use_custom_pkg_location'] = false
-default['opsworks_java']['jvm_pkg']['custom_pkg_location_url_debian'] = 'http://aws.amazon.com/'
-default['opsworks_java']['jvm_pkg']['custom_pkg_location_url_rhel'] = 'https://aws.amazon.com/'
+default['opsworks_java']['jvm_pkg']['use_custom_pkg_location'] = true
+default['opsworks_java']['jvm_pkg']['custom_pkg_location_url_debian'] = 'https://s3.amazonaws.com/opsworksbucket/jdk-7u55-linux-x64.rpm'
+default['opsworks_java']['jvm_pkg']['custom_pkg_location_url_rhel'] = 'https://s3.amazonaws.com/opsworksbucket/jdk-7u55-linux-x64.rpm'
 case node[:platform_family]
 when 'debian'
-  default['opsworks_java']['jvm_pkg']['name'] = "openjdk-#{node['opsworks_java']['jvm_version']}-jdk"
+  default['opsworks_java']['jvm_pkg']['name'] = "jdk-#{node['opsworks_java']['jvm_version']}-linux-x64.rpm"
 when 'rhel'
-  default['opsworks_java']['jvm_pkg']['name'] = "java-1.#{node['opsworks_java']['jvm_version']}.0-openjdk-devel"
+  default['opsworks_java']['jvm_pkg']['name'] = "jdk-#{node['opsworks_java']['jvm_version']}-linux-x64.rpm"
 end
 default['opsworks_java']['jvm_pkg']['java_home_basedir'] = '/usr/local'
 
